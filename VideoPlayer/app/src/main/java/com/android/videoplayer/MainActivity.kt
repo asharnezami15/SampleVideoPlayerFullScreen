c51fcb6ebf283c5da000c02843a22e8cdd9b02f9
asharnezami15 <52868509+asharnezami15@users.noreply.github.com>
2020-09-03 18:29:43 +0530

    Full screen mode
    If user has mobile navigation it will hide

--- VideoPlayer/app/src/main/java/com/android/videoplayer/MainActivity.kt
+++ VideoPlayer/app/src/main/java/com/android/videoplayer/MainActivity.kt
@@ -8,6 +8,7 @@ package com.android.videoplayer
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import android.widget.MediaController
 import android.widget.RelativeLayout
@@ -114,10 +115,10 @@ class MainActivity : AppCompatActivity() {
     private fun fullScreen() { // play in full screen
         isFullScreen = true
         iv_expand.setImageResource(R.drawable.fullscreen_exit)
-        window.setFlags(
+       /* window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        )*/
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         window.setFlags(
@@ -144,6 +145,32 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
 
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 
 }
